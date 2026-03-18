e03f832bdb5acdf7545f5c09ba419ed2fe0cb102
Luck-ai <67011698@kmitl.ac.th>
2026-03-18 15:58:19 +0700

    improved jcb

--- jcb.m
+++ jcb.m
@@ -1,4 +1,4 @@
-function [Z, O, jv, jw, jfinal] = jcb(n, t_all, robot)
+function [jv, jw, jfinal] = jcb(n, t_all, robot)
     Z = sym(zeros(3,n+1));
     O = sym(zeros(3,n+1));
 
@@ -16,7 +16,14 @@ function [Z, O, jv, jw, jfinal] = jcb(n, t_all, robot)
     for i = 1:n
         z_prev = Z(:, i);
         o_prev = O(:, i);
-
+        fprintf('Joint %d Matrix:\n', i);
+        simplify(t_all(i))
+        
+        fprintf('Z vector:\n'); 
+        disp(Z(:,i+1));
+        
+        fprintf('O vector:\n');
+        disp(O(:,i+1));
         if robot.links(i).isrevolute
             jv(:, i) = cross(z_prev, O(:,n+1) - o_prev);
             jw(:, i) = z_prev;
@@ -41,4 +48,4 @@ end
 % robot = SerialLink(L2);
 % [~, t_all] = fkine(robot, [th1 d2 thd d4]);
 
-% [Z, O, jv, jw, jf] = jcb(4, t_all, robot)
+% [jv, jw, jf] = jcb(4, t_all, robot)
